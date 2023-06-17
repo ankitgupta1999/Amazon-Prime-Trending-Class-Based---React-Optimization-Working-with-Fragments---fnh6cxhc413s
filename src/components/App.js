df2fd47dc68bc1183d6257b72f8e5ddbc6df1a99
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-06-17 20:59:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,73 @@
 
-import React, { Component } from "react";
-import '../styles/App.css';
+import React, { Component } from 'react';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      slides: props.slides,
-      index: 0,
+      currentSlideIndex: 0
     };
   }
 
+  handleNext = () => {
+    const { currentSlideIndex } = this.state;
+    const { slides } = this.props;
+    const lastIndex = slides.length - 1;
+
+    if (currentSlideIndex === lastIndex) return;
+
+    this.setState({ currentSlideIndex: currentSlideIndex + 1 });
+  };
+
+  handlePrev = () => {
+    const { currentSlideIndex } = this.state;
+
+    if (currentSlideIndex === 0) return;
+
+    this.setState({ currentSlideIndex: currentSlideIndex - 1 });
+  };
+
+  handleRestart = () => {
+    this.setState({ currentSlideIndex: 0 });
+  };
+
   render() {
-    
-    const { index, slides } = this.state;
+    const { slides } = this.props;
+    const { currentSlideIndex } = this.state;
+    const currentSlide = slides[currentSlideIndex];
+    const isFirstSlide = currentSlideIndex === 0;
+    const isLastSlide = currentSlideIndex === slides.length - 1;
 
     return (
-      <></>
+      <div>
+        <h1 data-testid="title">{currentSlide.title}</h1>
+        <p data-testid="text">{currentSlide.text}</p>
+
+        <button
+          data-testid="button-restart"
+          onClick={this.handleRestart}
+          disabled={isFirstSlide}
+        >
+          Restart
+        </button>
+
+        <button
+          data-testid="button-prev"
+          onClick={this.handlePrev}
+          disabled={isFirstSlide}
+        >
+          Prev
+        </button>
+
+        <button
+          data-testid="button-next"
+          onClick={this.handleNext}
+          disabled={isLastSlide}
+        >
+          Next
+        </button>
+      </div>
     );
   }
 }
